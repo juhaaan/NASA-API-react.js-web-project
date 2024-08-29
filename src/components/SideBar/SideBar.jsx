4ee25ae3596eff5c15c79ddb42221b21a969d6b4
juhaaan <iwantbelieve_kk@yahoo.com>
2024-08-29 17:03:56 +0300

    scss adjustments mostly

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,10 +3,13 @@ import "./SideBar.scss"
 export default function SideBar() {
 	return (
 		<div className='SideBar'>
-			<h2>Mars landscape</h2>
-			<div>
-				<p>discription</p>
-				<p>something in the way</p>
+			<div className='BackGroundOverLay'></div>
+			<div className='SideBarContent'>
+				<h2>Mars landscape</h2>
+				<div>
+					<p>discription</p>
+					<p>something in the way</p>
+				</div>
 			</div>
 		</div>
 	)
